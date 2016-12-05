c4a5fedf065625aa9a96f9f6fe16153092e907ae
Pavithra K <pksjce@gmail.com>
2016-12-05 22:58:09 +0530

    Add couple of basic test cases

--- tests/output.test.js
+++ tests/output.test.js
@@ -2,6 +2,22 @@ import {outputConfig} from '../src/handleAnswers.js';
 
 describe('Correct answers', () => {
   it('should have correct ans', () => {
-    expect(true).toBe(true);
+    let config;
+    answerList.forEach((answer,index) => {
+      config = outputConfig(answer);
+      const key = Object.keys(expectedConfigList[index])[0];
+      const val = expectedConfigList[index][key];
+      expect(config[key]).toEqual(val);
+    });
   })
 })
+
+const answerList = [
+  {entry: './index.js'},
+  {outputPath: './', outputFilename: 'bundle.js'}
+]
+
+const expectedConfigList = [
+  {entry: './index.js'},
+  {output: {path: './', filename: 'bundle.js'}}
+]
